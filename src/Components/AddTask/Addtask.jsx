09bbdcd645d2cf4d683688319a0862c274a655c2
amoskyalo <amoskyalo927@gmail.com>
2022-09-23 23:33:24 +0300

    Added the date and time inputs

--- src/Components/AddTask/Addtask.jsx
+++ src/Components/AddTask/Addtask.jsx
@@ -5,35 +5,39 @@ import { useState } from 'react'
 const Addtask = ( {onAdd, removeForm} ) => {
     const [text, setTask] = useState("")
     const [day, setDay] = useState("")
+    const [time, setTime] = useState("00:00")
     const [reminder, setReminder] = useState(false)
 
 
     const onSubmit = (e) => {
       e.preventDefault()
 
-      if(!text || !day){
+      if(!text || !day || !time){
         alert("Please add day and task")
         return
       }
 
-      onAdd({text, day, reminder})
+      onAdd({text, day, reminder, time})
 
       setTask("");
-      setDay("")
+      setDay("");
+      setTime("00:00")
       setReminder(false);
-
       removeForm()
     }
 
   return (
     <form onSubmit={onSubmit}>
-        <label>Task</label>
+        <label>Task <span>*</span></label>
         <input type="text" placeholder="Add task" value={text}
         onChange ={(e)=> setTask(e.target.value)}/>
 
-        <label>Day and Time</label>
+        <label>Day and Time <span>*</span></label>
+        <div className='time-date'>
         <input type="date" placeholder="Add day and time" value={day}
         onChange={(e)=> setDay(e.target.value)}/>
+        <input type="time" value={time} onChange={(e)=> setTime(e.target.value)}/>
+        </div>
 
         <label>Set reminder</label>
         <input type="checkbox" value={reminder}
