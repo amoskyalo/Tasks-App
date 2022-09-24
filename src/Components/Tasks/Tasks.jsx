fbe9cf6b57cac456876f93d9302959a6a4056c12
amoskyalo <amoskyalo927@gmail.com>
2022-09-24 15:29:19 +0300

    Minor changes

--- src/Components/Tasks/Tasks.jsx
+++ src/Components/Tasks/Tasks.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Task from '../Task/Task'
-
+import './Tasks.css'
 
 const Tasks = ( {tasks, onDelete, setReminder, addFavTask}) => {
   return (
-    <>
+    <div className='body'>
         {tasks.map( (task) => (
             <Task key={task.id} task={task} onDelete={onDelete} setReminder={setReminder}
             addFavTask={addFavTask}/>
         ))}
-    </>
+    </div>
   )
 }
 
